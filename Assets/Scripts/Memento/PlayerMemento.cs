f00baefe50a17a5593f2304249e640d824bf5449
Aaron Herman Choi <108954995+AaronHChoi@users.noreply.github.com>
2025-10-08 06:10:26 -0300

    Refactor Memento pattern and add player health
    Changed GameStatusManager to reference Originator instead of PlayerMovement and fixed variable naming in Simulacro.unity. Added a public health field to Originator and included health in the scene. Refactored PlayerMemento to be a plain class instead of MonoBehaviour, aligning with the Memento pattern.

--- Assets/Scripts/Memento/PlayerMemento.cs
+++ Assets/Scripts/Memento/PlayerMemento.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 namespace Memento
 {
-    public class PlayerMemento : MonoBehaviour
+    public class PlayerMemento
     {
         public Vector3 Position { get; private set; }
         public int Health {get; private set;}
 
-        // Update is called once per frame
         public PlayerMemento(Vector3 position, int health)
         {
             this.Position = position;
